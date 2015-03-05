1c785d7aa18cd4854ab716277d964b6cc76d2e87
Ian Jennings <ian@meetjennings.com>
2015-03-05 11:59:18 -0800

    update c3 version

---
+++ node_modules/c3/spec/c3-spec.js
@@ -0,0 +1,12 @@
+
+describe('c3', function () {
+    'use strict';
+
+    var c3 = window.c3;
+
+    it('exists', function () {
+        expect(c3).not.toBeNull();
+        expect(typeof c3).toBe('object');
+    });
+});
+
