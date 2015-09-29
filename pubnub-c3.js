1af7d6dd7547ca5d94f4ddd1fe40c32b5b2465ee
Ian Jennings <ian@meetjennings.com>
2015-09-28 19:12:08 -0500

    use pubnub datetime for all undefined x values

--- pubnub-c3.js
+++ pubnub-c3.js
@@ -5,12 +5,26 @@ eon.subsub = eon.subsub || subsub;
 eon.c = {
   create: function(options) {
 
+    options.debug = options.debug || false;
+
+    var clog = function(s, o, e) {
+
+      if(options.debug) {
+        if(e) {
+          console.log('EON-CHART:', s, o, e); 
+        } else {
+          console.log('EON-CHART:', s, o);
+        }
+      }  
+    };
+
+    clog('Status:', 'Initialized');
+
     var self = this;
     var error = false;
     var dateID = "_eon_datetime";
 
     self.chart = false;
-
     self.is_dead = false;
 
     self.pubnub = options.pubnub || PUBNUB || false;
@@ -21,12 +35,13 @@ eon.c = {
 
     options.transform = options.transform || function(m){return m};
     options.channel = options.channel || false;
-    options.generatge = options.generate || {};
+    options.generate = options.generate || {};
     if(!options.generate.data) {
       options.generate.data = {
         columns: null
       };
     }
+
     options.flow = options.flow || false;
     if(options.flow) { 
       if(typeof(options.flow) == "boolean") {
@@ -42,7 +57,7 @@ eon.c = {
 
     // x axis definition
     options.x_type = options.x_type || "datetime";
-    options.x_id = options.x_type_id || "";
+    options.x_id = options.x_id || "";
 
     if(options.x_type == "custom") {
 
@@ -77,28 +92,49 @@ eon.c = {
 
     }
 
+    clog('Options:', options);
 
     if(!options.channel) {
       error = "No channel supplied.";
     }
 
+    var appendDate = function(data, pubnub_date) {
+
+      if(options.x_type == "datetime") {
+        clog('PubNub:', 'Appending PubNub datetime to columns.');
+        var date = String(pubnub_date).slice(0,-7) * 1000;
+        data.push([dateID, new Date(date)]);
+      }
+
+      return data;
+       
+    }
+
     var all_messages = [];
+    var bigload = [];
     var page = function() {
+
+      clog('Status:', 'Restoring from history');
 
       all_messages = [];
       var i = 0;
 
       var getAllMessages = function(timetoken) {
+      
+        clog('History:', 'Retrieving messages from ' + timetoken);
 
          self.pubnub.history({
           count: options.limit,
           channel: options.channel,
           start: timetoken,
-           callback: function(payload) {
+          include_token: true,
+          callback: function(payload) {
 
              var msgs = payload[0];
              var start = payload[1];
              var end = payload[2];
+
+             clog('History:', msgs.length + ' messages found');
 
              if (msgs !== undefined && msgs.length) {
 
@@ -111,16 +147,30 @@ eon.c = {
              }
 
              if (msgs.length && all_messages.length < options.limit) {
-               getAllMessages(start);
+              getAllMessages(start);
              } else {
+
+                clog('History:', 'Complete... Rendering');
+                var loadArray = [];
 
                 i = 0;
                 while(i < all_messages.length) {
 
-                  render(all_messages[i].columns);
+                  var inArray = false;
+                  for(var j in loadArray) {
+
+                  }
+                  var a = all_messages[i];
+                  a = appendDate(a.message.columns, a.timetoken)
+                  bigload = storeData(a, bigload)
+
                   i++;
 
                 }
+
+                self.chart.load({
+                  columns: bigload
+                });
 
              }
 
@@ -157,24 +207,24 @@ eon.c = {
     var lastX = null;
     var dataStore = [];
 
-    var storeData = function(data) {
+    var storeData = function(data, target) {
 
       var i = 0;
-      if(!dataStore.length) {
-        dataStore = JSON.parse(JSON.stringify(data));
+      if(!target.length) {
+        target = JSON.parse(JSON.stringify(data));
       } else {
 
         while(i < data.length) {
 
           // if this is a new key, add the id
-          if(typeof dataStore[i] == "undefined") {
-            dataStore[i] = [data[i][0]];
+          if(typeof target[i] == "undefined") {
+            target[i] = [data[i][0]];
           }
 
-          dataStore[i].push(data[i][1]);
+          target[i].push(data[i][1]);
 
-          if(dataStore[i].length > options.limit) {
-            dataStore[i].splice(1,1);
+          if(target[i].length > options.limit) {
+            target[i].splice(1,1);
           }
 
           i++;
@@ -182,11 +232,13 @@ eon.c = {
 
       }
 
+      return target;
+
     }
 
     var kill = function() {
 
-      console.log('kill')
+      clog('Status:', 'Chart Animation Disabled');
 
       self.is_dead = true;
 
@@ -200,7 +252,7 @@ eon.c = {
 
     var boot = function() {
 
-      console.log('boot')
+      clog('Status:', 'Chart Animation Enabled');
       
       self.is_dead = false;
 
@@ -212,11 +264,6 @@ eon.c = {
 
       self.chart = c3.generate(options.generate);
 
-    };
-
-    var reboot = function() {
-      kill();
-      boot();
     };
 
     Visibility.change(function (e, state) {
@@ -231,27 +278,33 @@ eon.c = {
 
     var render = function(data) {
 
-      storeData(data);
+      clog('Status:', 'Rendering');
+
+      dataStore = storeData(data, dataStore);
 
       if(self.is_dead) {
 
-        console.log('got data while unfocused')
-        console.log(data)
+        clog('Render:', 'Tab out of focus.');
 
       } else {
 
         if(options.flow) {
+          
+          clog('Render:', 'Flow enabled, appending to chart.');
 
           var trimLength = needsTrim();
 
           if(trimLength)  {
             options.flow.length = 1;
+            clog('Render:', 'Data exceeds options.limit, trimming buffer.');
           }
 
           options.flow.columns = data;
           self.chart.flow(options.flow);
 
         } else {
+
+          clog('Render:', 'Updating chart.');
 
           self.chart.load({
             columns: data
@@ -268,21 +321,25 @@ eon.c = {
       if(options.history) {
         page();
       }
+      
+      clog('PubNub:', 'Subscribed to ' + options.channel);
 
       subsub.subscribe(self.pubnub, options.channel, options.connect, function(message, env, channel) {
 
+        clog('PubNub:', '-------------------');
+        clog('PubNub:', 'Received Message', message);
+
         var message = options.transform(message);
 
-        options.message(message, env, channel);
+        clog('PubNub:', 'Transforming Message using options.transform');
+        message.columns = appendDate(message.columns, env[1]);
 
-        console.log(options.x_type)
-        if(options.x_type == "datetime") {
-          message.columns.push([dateID, new Date().getTime()]);
-        }
-
-        console.log(message.columns)
-
+        clog('PubNub:', 'Message Result', message);
+      
         render(message.columns);
+
+        clog('PubNub:', 'Calling options.message');
+        options.message(message, env, channel);
 
       });
 
